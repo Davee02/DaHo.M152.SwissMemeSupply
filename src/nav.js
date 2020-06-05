d19361149905c6cb7feecf7bb19eda0d0e79c198
David <davidhodel6@gmail.com>
2020-06-05 23:15:29 +0200

    add responsive design

--- src/nav.js
+++ src/nav.js
@@ -1,5 +1,5 @@
 function showBurger() {
-  var x = document.getElementById("myLinks");
+  var x = document.getElementById("navLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
